6ca8b9320afe462f7eb35621642def4a95bf1d1c
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2024-03-30 20:13:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Udeshya";
+const lName = "Sharma";
+ReactDOM.render(
+  <h1>
+    Hello {fName} {lName}!
+  </h1>,
+  document.getElementById("root")
+);
